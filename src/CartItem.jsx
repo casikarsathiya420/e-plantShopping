9db17d1f2e3885dd966d23d8a896bfd714926982
Sathiya Narayanan <sathiyacasikar1992@gmail.com>
2025-10-05 12:57:16 -0400

    Fixed minor bug

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -48,7 +48,7 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleCheckoutShopping = (e) => {
-        alert('Functionality to be added for future reference');
+        alert('Coming soon');
     };
 
     // Calculate total cost based on quantity for an item
@@ -83,7 +83,7 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
             </div>
         </div>
     );
